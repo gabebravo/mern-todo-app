c54122a19da18fc9b05c49bc126099386d9b2535
Gabe Bravo <gabecbravo@gmail.com>
2017-08-24 19:01:47 -0400

    Added the build folder as well as the travis.yml file.

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ if (env === 'development') {
   process.env.MONGODB_URI = 'mongodb://localhost:27017/todos'; // set local db
 } else if ( env === 'test') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/todos_test';
+  process.env.MONGODB_URI = process.env.TEST_DB;
 } else if (env === 'localtest') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/todos_test'; // set local db
